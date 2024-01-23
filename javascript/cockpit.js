5bf96b60196574aecf49b2c77d1ef3b8a5e5bb10
HOZ <haluk.oezduman@opitz-consulting.com>
2024-01-23 09:37:47 +0100

    all of them are working

--- javascript/cockpit.js
+++ javascript/cockpit.js
@@ -19,7 +19,6 @@ var dogMood = "no dog";
 var pedestrianInfo = "approaching car";
 
 var ambientTemp = 0;
-var ambientTempChange = ()=>{};
 
 
 
@@ -39,13 +38,12 @@ simulator("Vehicle.Cabin.HVAC.AmbientAirTemperature", "get", () => {
     return ambientTemp;
 })
 
+simulator("Vehicle.Exterior.PedestrianApproach", "get", () => {
+    return (pedestrianInfo === "pulling handle 3x");
+})
+
 simulator("Vehicle.Cabin.HVAC.AmbientAirTemperature", "subscribe", (callback) => {
-    console.log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-    console.log(typof(callback));
-    if (typeof(callback) == "function"){
-        console.log("FUNCTION");
-        ambientTempChange = callback;
-    }
+    console.log(callback);
 })
 
 simulator("Vehicle.Exterior.PedestrianApproach", "subscribe", (callback) => {
@@ -84,6 +82,7 @@ function updateDogMood () {
     if (div.querySelector('#happy').checked) {
         div.querySelector("#happydogframe").src = "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fcalm.gif?alt=media&token=7c01a791-1be9-402b-8609-094ed3ee88e1";
         dogMood = "happy";
+        
     } else if(div.querySelector('#sad').checked) {
         div.querySelector("#happydogframe").src = "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fwild.gif?alt=media&token=ac5dbeaf-6fb4-4efb-a4b8-817c18bc9662";
         dogMood = "sad";
@@ -91,6 +90,7 @@ function updateDogMood () {
         div.querySelector("#happydogframe").src = "https://media.digitalauto.tech/data/images/0d7f20bd-087a-4163-afe9-efcda5f23a11nodog-im-auto.jpg";
         dogMood = "no dog";
     }
+    //container.querySelector("#dog-photo").src = div.querySelector("#happydogframe").src;
 }
 
 function updateTermo() {
@@ -99,15 +99,25 @@ function updateTermo() {
 	    "very-hot": "11px",
 	    hot: "90px",
 	    warm: "160px",
-	    cold: "230px",
-	    "very-cold": "300px"
+	    cold: "230px"
     };
 
     let tempFill = thermometerDiv.querySelector(".thermometer-body-fill");
     let controls = thermometerDiv.querySelector(".controls");
 
     controls.addEventListener("click", (event) => {
+
 	if (event.target.nodeName === "BUTTON") {
+
+        if(event.target.id === 'very-hot') {
+            ambientTemp = 40;
+        } else if(event.target.id === 'hot') {
+            ambientTemp = 30;            
+        } else if(event.target.id === 'warm') {
+            ambientTemp = 20;
+        } else {
+            ambientTemp = 10;
+        }
 		tempFill.style.setProperty("top", FILL_HEIGHT[event.target.id]);
 	}
     });
@@ -124,21 +134,40 @@ function updatePedestrianImage() {
         pedestrianDiv.querySelector("#pedestrian-image").src = "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fpull%201.gif?alt=media&token=e9b6296b-3c14-4322-85c9-7ce74bceca8c";
     } else {
         pedestrianInfo = "no one"; 
-        pedestrianDiv.querySelector("#pedestrian-image").src = "https://media.digitalauto.tech/data/images/fb236bdc-c719-4473-8fbb-3b18718212c1noone.jpg";
+        pedestrianDiv.querySelector("#pedestrian-image").src = "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FMicrosoftTeams-image%20(1).png?alt=media&token=d17841fa-24ce-407e-a3ee-c54c6e372839";
     }
 
 }
 
-function updateCarWindowImage() {
+function showPedestrianMessage(pullInfo) {
 
-    if (carOutsideDiv.querySelector('#car-window-open-window').checked){
+    if (pullInfo == "pulling") {
+        carOutsideDiv.querySelector("#car-window-text").style.visibility = 'visible';
+    } else {
+        carOutsideDiv.querySelector("#car-window-text").style.visibility = 'hidden';
+    }
+}
+
+
+function updateCarWindowImage(movement) {
+
+/*    if (pedestrianInfo === "pulling") {
+        carOutsideDiv.querySelector("#car-window-text").style.visibility = 'visible';
+    } else {
+        carOutsideDiv.querySelector("#car-window-text").style.visibility = 'hidden';
+    }*/
+
+    if (movement === "open"){
+        klimaDiv.querySelector("#klima-image-above").src = "https://media.digitalauto.tech/data/images/75cc03ae-8ff8-4103-a364-fca34385f94acold.png";
         carOutsideDiv.querySelector("#car-window-image").src = "https://media.digitalauto.tech/data/images/70a0f85a-5e79-4749-9922-9dbec48ada53open.jpg";
     
-    } else {
+    } else if(movement === "close") {
+        klimaDiv.querySelector("#klima-image-above").src = "https://media.digitalauto.tech/data/images/81c6801e-7e78-4888-b0c3-fd3554febbb4warm.png";
         carOutsideDiv.querySelector("#car-window-image").src = "https://media.digitalauto.tech/data/images/74b79804-c5e4-4b93-95ec-230f51139feaclose.png";
     } 
 }
 
+/*
 async function tempSimStep () {
     const fan1Temp = await vehicle["Cabin.HVAC.Station.Row2.Right.Temperature"].get();
     const fan2Temp = await vehicle["Cabin.HVAC.Station.Row2.Left.Temperature"].get();
@@ -152,7 +181,7 @@ async function tempSimStep () {
     ambientTempChange(ambientTemp);
 }
 
-setInterval(tempSimStep, 1000)
+setInterval(tempSimStep, 1000)*/
 
 function updateDiv (text, div) {
     fetch(text).then(r=>r.text()).then(t=>div.innerHTML = t).then(() => {div.querySelector("#dog-mode-radio-buttons").addEventListener("change", updateDogMood)});
@@ -171,15 +200,15 @@ function updatePedestrian (text, pedestrianDiv) {
 function updateCarOutside (text, carOutsideDiv) {
 
     fetch(text).then(r=>r.text()).then(t=>carOutsideDiv.innerHTML = t).then(() => {
-        carOutsideDiv.querySelector('#car-window-open-radio-buttons').addEventListener("change", function() {
+      /*  carOutsideDiv.querySelector('#car-window-open-radio-buttons').addEventListener("change", function() {
             updateCarWindowImage();
-        });
+        }); */
     }); 
 } 
 
 function updateThermo (text, thermometerDiv) {
-     fetch(text).then(r=>r.text()).then(t=>thermometerDiv.innerHTML = t).then(() => {thermometerDiv.querySelector(".controls").addEventListener("click", function () {
-        updateTermo()  })});
+     fetch(text).then(r=>r.text()).then(t=>thermometerDiv.innerHTML = t).then(() => {
+        updateTermo() });
 }
 
 function updateRadio (text, radioDiv) {
@@ -238,30 +267,13 @@ widgets.register("Mobile", (box) => {
 	}))
 });
 
+const container = document.createElement("div")
 
- return {
-        dog_mood : () => {
-            return dogMood;
-        },
-        pedestrian_info: ()  => {
-            return pedestrianInfo;
-        },
-        notifyPhone: (message) => {
-            mobileNotifications(message)
-        },
-        streamDialoguePhone: (camera) => {
-            openDialog(camera)
-        }
-    }
-}
-
-export default plugin
-
-
-const MobileNotifications = ({box, apis = null, vehicle = null, refresh = null, paddingTop = 25, paddingHorizontal = 12, backgroundColor = null}) => {
+const MobileNotifications = ({box, apis = null, vehicle = null, refresh = null, paddingTop = 25, paddingHorizontal = 12, backgroundColor = null}, dog_mood) => {
     
     let allMessages = "";
-    const container = document.createElement("div")
+    let isCameraOpened = false;
+  
     container.setAttribute("style", `height: 100%; width: 100%;`)
     container.innerHTML = (`
     <style>
@@ -281,25 +293,65 @@ const MobileNotifications = ({box, apis = null, vehicle = null, refresh = null, 
 		</style>
     <div style="max-width: fit-content; margin: 0 auto; position: relative;">
     <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FDashboardPhone.png?alt=media&token=d361018a-b4b3-42c0-8ef0-16c9e70fd9c7" style="height: 95%; width: 100%; object-fit: contain;">
-        <div class="smartphone-text" style="position: absolute; color: white; font-family: 'Lato'; width: 100%; top: 0; height: 95%; box-sizing: border-box; padding-top: ${paddingTop}px; padding-right: ${paddingHorizontal}px; padding-left: ${paddingHorizontal}px; padding-bottom: 25px; white-space: break-spaces;"></div>
+        <div class="smartphone-text" style="position: absolute; color: white; font-weight: bold; font-family: 'Lato'; width: 100%; top: 0; height: 95%; box-sizing: border-box; padding-top: ${paddingTop}px; padding-right: ${paddingHorizontal}px; padding-left: ${paddingHorizontal}px; padding-bottom: 25px; white-space: break-spaces;"></div>
     </div>
-    <img id="dog-photo" src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fwild.gif?alt=media&token=ac5dbeaf-6fb4-4efb-a4b8-817c18bc9662" width="300" height="300" style="position: relative; top: -250px"></img>
-    <p><i>Do you want to see you dog</i></p>
-    <button class="button">Open Camera</button>
-    <input type="text" id="message" onkeydown="search(this)" style="position: relative; top: -120px; left: -40px" name="message">
+    <img id="dog-photo-happy" src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fcalm.gif?alt=media&token=7c01a791-1be9-402b-8609-094ed3ee88e1" width="340" height="300" style="position: relative; top: -550px; margin-left: 28px; display:none"></img>
+    <img id="dog-photo-sad" src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fwild.gif?alt=media&token=ac5dbeaf-6fb4-4efb-a4b8-817c18bc9662" width="340" height="300" style="position: relative; top: -550px; margin-left: 28px; display:none"></img>
+    <img id="dog-photo-no" src="https://media.digitalauto.tech/data/images/0d7f20bd-087a-4163-afe9-efcda5f23a11nodog-im-auto.jpg" width="340" height="300" style="position: relative; top: -550px; margin-left: 28px; display:none"></img>
+    <p style="position: relative; top: -550px" ><i>Do you want to see your dog?</i></p>
+    <button class="button" id="openCameraId" style="position: relative; top: -520px; background-color: #04AA6D; border: none; color: white; padding: 5px 25px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;">Open Camera</button>
+    <button class="button" id="closeCameraId" style="position: relative; top: -520px; background-color: #008CBA; border: none; color: white; padding: 5px 25px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;" onclick="closeCamera()">Close Camera</button>
+    
     `)
     box.injectNode(container)
 
- //   container.querySelector("#photo-btn").addEventListener("click", function(){ container.querySelector("#dog-photo").style.visibility = 'visible' });
-    container.querySelector("#message").addEventListener("keypress", updateValue);
-
+   // container.querySelector("#message").addEventListener("keypress", updateValue);
+    container.querySelector("#openCameraId").addEventListener("click", openCamera);
+    container.querySelector("#closeCameraId").addEventListener("click", closeCamera);
+/*
     function updateValue(e) {
         if (e.key === 'Enter') {
             allMessages = e.target.value + "\n";
             container.querySelector(".smartphone-text").textContent = allMessages;
             container.querySelector("#message").value = '';
         }
+    }
+*/
+    function updateImage() {
+      if (isCameraOpened) {
+        if (dogMood === 'happy') {
+         //     container.querySelector("#dog-photo").src = "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fcalm.gif?alt=media&token=7c01a791-1be9-402b-8609-094ed3ee88e1";
+            container.querySelector("#dog-photo-happy").style.display = 'block';
+            container.querySelector("#dog-photo-sad").style.display = 'none';
+            container.querySelector("#dog-photo-no").style.display = 'none';
+      
+        } else if (dogMood === 'sad') {
+            container.querySelector("#dog-photo-sad").style.display = 'block';
+            container.querySelector("#dog-photo-happy").style.display = 'none';
+            container.querySelector("#dog-photo-no").style.display = 'none';
+         //     container.querySelector("#dog-photo").src = "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fwild.gif?alt=media&token=ac5dbeaf-6fb4-4efb-a4b8-817c18bc9662";
+        } else {
+            container.querySelector("#dog-photo-no").style.display = 'block';
+           container.querySelector("#dog-photo-sad").style.display = 'none';
+            container.querySelector("#dog-photo-happy").style.display = 'none';
+        //      container.querySelector("#dog-photo").src = "https://media.digitalauto.tech/data/images/0d7f20bd-087a-4163-afe9-efcda5f23a11nodog-im-auto.jpg";
+        }
+      }
+    
+    }
 
+    function openCamera() {
+        isCameraOpened = true;
+        setInterval(updateImage, 100);
+
+    }
+
+    
+    function closeCamera(e) {
+        isCameraOpened = false;
+        container.querySelector("#dog-photo-happy").style.display = 'none';
+        container.querySelector("#dog-photo-sad").style.display = 'none';
+        container.querySelector("#dog-photo-no").style.display = 'none';
     }
 
     const updateNotification = async () => {
@@ -314,6 +366,7 @@ const MobileNotifications = ({box, apis = null, vehicle = null, refresh = null, 
         }                    
     }
 
+
     if (refresh !== null) {
         if (typeof refresh !== "number") {
             throw new Error("parameter 'refresh' must be an error")
@@ -324,17 +377,40 @@ const MobileNotifications = ({box, apis = null, vehicle = null, refresh = null, 
 
     return {
         printNotification: (message) => {
-
             if(message !== undefined || message !== "") {
                 allMessages = message + "\n";
                 container.querySelector(".smartphone-text").textContent = allMessages
-            }            
+            }        
         }
     }
 
 };
 
-const openDialog = () => {
 
+ return {
+        dog_mood : () => {
+            return dogMood;
+        },
+        pedestrian_info: ()  => {
+            return pedestrianInfo;
+        },
+        notifyPhone: (message, dog_mood) => {
+            mobileNotifications(message, dog_mood)
+        },
+        streamDialoguePhone: (callback) => {
+            console.log(callback)
+        },
+        controlWindow: (movement) => {
+            updateCarWindowImage(movement)
+        },
+        showPedestrianMessage: (pullInfo) => {
+            showPedestrianMessage(pullInfo);
+        }
+    }
 }
+
+export default plugin
+
+
+
   
